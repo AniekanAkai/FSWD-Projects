7f27bbd223f8452fddec35dd34e4f84984269804
Aniekan Akai <aniekanakai@cmail.carleton.ca>
2015-08-26 01:04:42 -0400

    working version

---
+++ media.py
@@ -0,0 +1,8 @@
+import os
+import sys
+
+class Movie:
+	def __init__(self, title, posterUrl, trailerUrl):
+		self.title = title
+		self.posterUrl = posterUrl
+		self.trailerUrl = trailerUrl
---
+++ readme.txt
@@ -0,0 +1,2 @@
+Steps to run program:
+1. Run entertainment_center.py in IDLE(or any other python IDE)
