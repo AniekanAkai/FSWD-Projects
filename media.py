afcad0c6f59618545e049922227e053baebe3a6a
Aniekan Akai <aniekanakai@cmail.carleton.ca>
2015-08-28 00:27:34 -0400

    updated according to review notes

--- media.py
+++ media.py
@@ -1,14 +1,17 @@
-#Author: Aniekan Akai
-#Contains all media related objects
+# Author: Aniekan Akai
+# Contains all media related objects
 
+#Libraries to be used are imported
 import os
 import sys
+
 ################################################
-#Movie object containing the following fields: #
+# Movie object containing the following fields: #
 #	1. Title								   #
 #	2. Poster URL							   #
 #	3. Trailer YouTube URL					   #
 ################################################
+
 class Movie:
 	def __init__(self, title, posterUrl, trailerUrl):
 		self.title = title
