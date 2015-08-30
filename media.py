e021d492a03b742b9197f4eea80ea6d51f8afb1c
Aniekan Akai <aniekanakai@cmail.carleton.ca>
2015-08-30 01:46:29 -0400

    Attemptno 3 on PEP008 update

--- media.py
+++ media.py
@@ -5,14 +5,18 @@
 import os
 import sys
 
-################################################
-# Movie object containing the following fields: #
-#	1. Title								   #
-#	2. Poster URL							   #
-#	3. Trailer YouTube URL					   #
-################################################
+
+
 
 class Movie:
+	"""
+	Movie class to hold information about the favourite movies
+	A movie object containing the following fields: 
+	1. Title								   
+	2. Poster URL							   
+	3. Trailer YouTube URL					   
+
+	"""
 	def __init__(self, title, posterUrl, trailerUrl):
 		self.title = title
 		self.posterUrl = posterUrl
