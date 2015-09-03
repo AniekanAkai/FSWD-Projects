feddb5dd88cde7d42d5a73a0e9b7a59213d58e97
Aniekan Akai <aniekanakai@cmail.carleton.ca>
2015-09-02 21:38:19 -0400

    Updated with PEP-008

--- media.py
+++ media.py
@@ -1,23 +1,20 @@
 # Author: Aniekan Akai
 # Contains all media related objects
 
-#Libraries to be used are imported
+# Libraries to be used are imported
 import os
 import sys
 
 
-
-
 class Movie:
-	"""
-	Movie class to hold information about the favourite movies
-	A movie object containing the following fields: 
-	1. Title								   
-	2. Poster URL							   
-	3. Trailer YouTube URL					   
-
-	"""
-	def __init__(self, title, posterUrl, trailerUrl):
-		self.title = title
-		self.posterUrl = posterUrl
-		self.trailerUrl = trailerUrl
+    """
+    Movie class to hold information about the favourite movies
+    A movie object containing the following fields:
+    1. Title
+    2. Poster URL
+    3. Trailer YouTube URL
+    """
+    def __init__(self, title, posterUrl, trailerUrl):
+        self.title = title
+        self.posterUrl = posterUrl
+        self.trailerUrl = trailerUrl
